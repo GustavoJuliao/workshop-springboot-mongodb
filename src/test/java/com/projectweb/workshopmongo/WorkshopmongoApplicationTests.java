ea716c91a42eec19194359564bf1f97c547beb8c
GustavoSouza <gj.juliao>
2022-03-23 11:57:43 -0300

    Entity user e rest funcionando

---
+++ src/test/java/com/projectweb/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectweb.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
