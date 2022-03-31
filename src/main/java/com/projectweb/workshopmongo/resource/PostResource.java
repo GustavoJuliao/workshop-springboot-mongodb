1d93e81a9262d0bd78f768b73d886c57e62eede3
GustavoSouza <gj.juliao>
2022-03-31 10:04:40 -0300

    Consulta simples com query methods / @query

--- src/main/java/com/projectweb/workshopmongo/resource/PostResource.java
+++ src/main/java/com/projectweb/workshopmongo/resource/PostResource.java
@@ -1,25 +1,37 @@
 package com.projectweb.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projectweb.workshopmongo.domain.Post;
+import com.projectweb.workshopmongo.resource.util.URL;
 import com.projectweb.workshopmongo.services.PostService;
 
 @RestController
-@RequestMapping(value="/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
 	@Autowired
 	private PostService service;
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
- 	public ResponseEntity<Post> findById(@PathVariable String id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
--- src/main/java/com/projectweb/workshopmongo/services/PostService.java
+++ src/main/java/com/projectweb/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.projectweb.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ public class PostService {
 
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
-		return post.orElseThrow(() ->  new ObjectNotFoundException("Objeto não encontrado."));
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
 	}
+
+	public List<Post> findByTitle(String title) {
+		return repo.searchTitle(title);	
+		}
 }
